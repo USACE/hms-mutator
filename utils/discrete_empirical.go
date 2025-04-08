3e94b0b3f57d1ef5af9536191f60321ac9e3d68e
Will Lehman <william.p.lehman@usace.army.mil>
2025-04-08 13:36:32 -0400

    fixing discrete emperical

--- utils/discrete_empirical.go
+++ utils/discrete_empirical.go
@@ -9,31 +9,34 @@ type DiscreteEmpiricalDistribution struct {
 	bin_starts             []int
 	cumulative_probability []float64
 }
-func NewDescreteEmpiricalDistribution(bin_starts int[], cumuatlive_probs []float64) DiscreteEmpiricalDistribution{
+
+func NewDescreteEmpiricalDistribution(bin_starts []int, cumuatlive_probs []float64) DiscreteEmpiricalDistribution {
 	return DiscreteEmpiricalDistribution{bin_starts: bin_starts, cumulative_probability: cumuatlive_probs}
 }
-func DescreteEmpiricalDistributionFromBytes(data []byte) DiscreteEmpiricalDistribution{
+func DescreteEmpiricalDistributionFromBytes(data []byte) DiscreteEmpiricalDistribution {
 	stringbytes := string(data)
-	lines := strings.Split(data,"\r\n")
-	starts := make([]int)
-	probs := make([]probs)
+	lines := strings.Split(stringbytes, "\r\n")
+	starts := make([]int, 0)
+	probs := make([]float64, 0)
 	var dist DiscreteEmpiricalDistribution
-	for i, line := range lines{
-		if i>0{
-			if len(line)>0{
+	for i, line := range lines {
+		if i > 0 {
+			if len(line) > 0 {
 				vals := strings.Split(line, ",")
 				binstart, err := strconv.Atoi(vals[0])
-				if err!=nil{
+				if err != nil {
 					return dist
 				}
-				prob, err := strconv.ParseFloat(vals[1], 64)//if incrimental, update this val with time.
-				if err!=nil{
+				starts = append(starts, binstart)
+				prob, err := strconv.ParseFloat(vals[1], 64) //if incrimental, update this val with time.
+				if err != nil {
 					return dist
 				}
+				probs = append(probs, prob)
 			}
 		}
 	}
-	return NewDescreteEmpiricalDistribution(starts,probs)
+	return NewDescreteEmpiricalDistribution(starts, probs)
 }
 func (ded DiscreteEmpiricalDistribution) Sample(probability float64) int {
 	if ded.cumulative_probability[0] < probability {
